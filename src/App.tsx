dba1751c2da176795d6510731170e0be5ca30774
Horacaz <xistir97@gmail.com>
2023-04-23 07:07:22 -0300

    Adds preeliminar file structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+import getCityLocation from './hooks/getCityKey'
+function App() {  
+  const [query, setQuery] = useState('');
+  const [data, setData] = useState('');
 
-function App() {
-  const [count, setCount] = useState(0)
+  function searchQuery(event: Event): void{
+  event.preventDefault();
+  setData(getCityLocation(query));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World</h1>
+      <form onSubmit={(submit) => searchQuery(submit)}>
+      <label>
+      City Location Search
+      <input onChange={(input) => setQuery(input.target.value) } type='text'/>
+      </label>
+      <button type="submit">Get City Location</button>
+      </form>
+      <h2>City: key {data}</h2>
     </>
   )
 }
