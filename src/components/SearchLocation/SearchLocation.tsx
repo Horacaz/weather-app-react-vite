3cf10f756fdb3c8fceeb2210548d0af5169d2cb7
Horacaz <xistir97@gmail.com>
2023-05-17 18:28:31 -0300

    Removes jsx fragment from SearchLocation component.

--- src/components/SearchLocation/SearchLocation.tsx
+++ src/components/SearchLocation/SearchLocation.tsx
@@ -3,13 +3,11 @@ import { SearchInput, Label, Button, Container } from "./styles";
 export default function SearchLocation(props){
     const {onChange, onClick} = props;
     return(
-        <>
         <Container>
             <Label> Search Location
                 <SearchInput onChange={onChange}/>
             </Label>
             <Button onClick={onClick}> Fetch City</Button>
         </Container>
-        </>
     )
 }
