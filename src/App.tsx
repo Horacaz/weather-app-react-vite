1f0f33e77029e7d6880c3e8a530dfcedcb6b00bb
Horacaz <xistir97@gmail.com>
2023-05-17 18:34:37 -0300

    Implements CurrentForecast Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,30 @@ import "./App.css";
 import useCityLocation from "./hooks/useCityLocation";
 import useForecast from "./hooks/useForecast";
 import SearchLocation from "./components/SearchLocation/SearchLocation";
+import CurrentForecast from "./components/CurrentForecast/CurrentForecast";
 function App() {
   const { cityKey, setCityFetch } = useCityLocation();
-  const { setKey } = useForecast();
+  const { currentForecast, setKey } = useForecast();
   const [query, setQuery] = useState("");
 
   const onChangeCallBack = (e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value);
   const onClickCallBack = () => setCityFetch(query);
 
-  /*
     // fetches Forecast
     useEffect( () => {
       if(cityKey){
         setKey(cityKey);
       }
     }), [setKey, cityKey];
- */
+
   return (
     <>
       <h1>Weather in your Location</h1>
       <SearchLocation onChange = { onChangeCallBack } onClick = { onClickCallBack }/>
+      {currentForecast && (
+          <CurrentForecast title= "Current Forecast" CurrentForecast={currentForecast}/>
+      )}
+   
     </>
   );
 }
--- src/components/CurrentForecast/CurrentForecast.tsx
+++ src/components/CurrentForecast/CurrentForecast.tsx
@@ -1,10 +1,13 @@
-import styled from 'styled-components'
-const Title = styled.h1`
-font-size: 1.5em;
-text-align: center;
-color:palevioletred;`
-export default function CurrentForecast(){
+import { Container, Span} from "./styles";
+export default function CurrentForecast(props){
+    const {title, CurrentForecast} = props;
+
 return(
-    <Title>Hello World</Title>
+    <Container>
+         <h2>{title}</h2>
+         <p>Weather Description: <Span>{CurrentForecast.weatherDescription}</Span></p>
+         <p>Current Temperature: <Span>{CurrentForecast.temperature}</Span></p>
+         <p>Time of medition: <Span>{CurrentForecast.time}</Span></p>
+    </Container>
 )
 }
