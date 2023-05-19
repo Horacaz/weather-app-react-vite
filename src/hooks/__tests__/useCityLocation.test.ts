d82504ed3819982603bf3cf306c2830632910989
Horacaz <xistir97@gmail.com>
2023-05-19 16:01:29 -0300

    Adds custom hooks tests boilerplates.

---
+++ src/hooks/__tests__/useCityLocation.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from 'vitest';
+import useCityLocation from '../useCityLocation';
+describe("useCityLocation", () => {
+    test.skip("useCityLocation", () => {
+        useCityLocation();
+        expect(1 + 1 ).toBe(2);
+    })
+})
---
+++ src/hooks/__tests__/useForecast.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from 'vitest';
+import useForecast from '../useForecast';
+describe("useCityLocation", () => {
+    test.skip("useCityLocation", () => {
+        useForecast();
+        expect(1 + 1 ).toBe(2);
+    })
+})
